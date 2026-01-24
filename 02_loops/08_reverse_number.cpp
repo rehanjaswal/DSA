69d071541a28aa81bd1fcdf69e910b10609f2460
rehanjaswal <rehanxjaswal@gmail.com>
2026-01-24 18:46:58 +0530

    edge case

--- 02_loops/08_reverse_number.cpp
+++ 02_loops/08_reverse_number.cpp
@@ -4,6 +4,7 @@ int main() {
     int n;
     cout << "enter a number : ";
     cin >> n;
+    if (n == 0) cout << n;     // edge case 0
     while (n != 0) {
         cout << n % 10;
         n = n / 10;
