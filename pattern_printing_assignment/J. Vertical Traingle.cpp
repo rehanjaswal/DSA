fd2288e1789f1fa8a66656b69b017d2b22fcb213
rehanjaswal <rehanxjaswal@gmail.com>
2026-01-29 19:35:56 +0530

    patterns

--- pattern_printing_assignment/J. Vertical Traingle.cpp
+++ pattern_printing_assignment/J. Vertical Traingle.cpp
@@ -1,20 +1,38 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n - 1; i++) {
+//         for (int j = 1; j <= i; j++) {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+//     for (int i = n; i >= 1; i--) {
+//         for (int j = 1; j <= i; j++) {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 int main() {
     int n;
     cin >> n;
-    for (int i = 1; i <= n - 1; i++) {
-        for (int j = 1; j <= i; j++) {
+    for (int i = 1; i <= 2 * n - 1; i++) {
+        int stars;
+        if (i <= n) stars = i;
+        else stars = 2 * n - i;
+        for (int j = 1; j <= stars; j++ ) {
             cout << "* ";
         }
         cout << endl;
-    }
-    for (int i = n; i >= 1; i--) {
-        for (int j = 1; j <= i; j++) {
-            cout << "* ";
         }
-        cout << endl;
-    }
+
     return 0;
 }
 
