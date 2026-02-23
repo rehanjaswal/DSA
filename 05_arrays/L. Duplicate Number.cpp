c362c749d12ef187dbca2deddfdc55ce6a20730c
rehanjaswal <rehanxjaswal@gmail.com>
2026-02-23 23:08:15 +0530

    array galore

---
+++ 05_arrays/L. Duplicate Number.cpp
@@ -0,0 +1,28 @@
+// in an array, each element appears twice but one, find that particular element
+#include <iostream>
+using namespace std;
+int main() {
+    int n; 
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+    if (count == 2) {
+        cout << arr[i];
+        break;
+        }
+    }
+    return 0;
+}
