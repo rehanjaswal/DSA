12fab623eed5d2bfecdcb4c2da4af2cced126579
rehanjaswal <rehanxjaswal@gmail.com>
2026-02-27 20:26:03 +0530

    cool array problem

--- 06_2D_arrays/08_max_1s.cpp
+++ 06_2D_arrays/08_max_1s.cpp
@@ -1,4 +1,4 @@
-//   1 0 0 1     in this example, answer is 2 as row number two contains the highest number of ones 
+//   1 0 0 1     in this example, answer is 2 as row number 2 contains the highest number of 1s 
 //   1 1 0 1
 //   1 1 1 1
 //   0 0 0 1
@@ -17,25 +17,21 @@ int main() {
         }
     }
 
-    int maxRowIndex = 0, oneCountGlobal;
-
-    for (int i = 0; i < n; i++) {
+    int maxOneRow = 0, maxOneCount = 0;
+    for (int i = 0; i < n; i ++) {
         int oneCount = 0;
         for (int j = 0; j < m; j++) {
             if (arr[i][j] == 1) {
                 oneCount++;
-                cout << i;
             }
-        if (i > maxRowIndex) {
-            maxRowIndex = i;
         }
-        
-
-
+        if (oneCount > maxOneCount) {
+            maxOneCount = oneCount;
+            maxOneRow = i;
         }
     }
 
+    cout << maxOneRow << " " << maxOneCount;
     
-
     return 0;
 }
