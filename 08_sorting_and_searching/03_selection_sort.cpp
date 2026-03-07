b2d681d5edb91b067c78cf4e09267c6ce2c14b8e
rehanjaswal <rehanxjaswal@gmail.com>
2026-03-07 10:28:16 +0530

    selection sort method

---
+++ 08_sorting_and_searching/03_selection_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        int min = a[i];
+        int lcn = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (a[j] < min) {
+                min = a[j];
+                lcn = j;
+            }
+        }
+        swap(a[i], a[lcn]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
+
