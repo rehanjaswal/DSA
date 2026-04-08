b92cd3498fcc87d472c08e89a89442f0991d2f1e
rehanjaswal <rehanxjaswal@gmail.com>
2026-04-09 02:05:47 +0530

    sorting techniques and shit

---
+++ 08_sorting_and_searching/03_selection_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        int minimumElement = arr[i];
+        int locationMin = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if(arr[j] < minimumElement) {
+                minimumElement = arr[j];
+                locationMin = j;
+            }
+        }
+
+        swap(arr[i], arr[locationMin]);
+
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+    
+}
---
+++ 08_sorting_and_searching/04_bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+        bool swapped = false;
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swapped = true;
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+        if (swapped == false) {
+            break;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0; 
+}
