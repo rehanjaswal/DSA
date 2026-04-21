a741c18f7e864d8104843874b042f724bb0dc02a
rehanjaswal <rehanxjaswal@gmail.com>
2026-04-21 20:59:28 +0530

    intro to time complexity

---
+++ 10_time_space_complexity/01_factors.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;    
+int main() {
+    int n;
+    cin >> n;
+    
+    int count = 0;
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            int j = n / i;
+            
+            if (i == j) {
+                count++;
+            }
+            else {
+                count += 2;
+            }
+        }
+    }
+    
+    cout << count << endl;
+
+    return 0;
+}
