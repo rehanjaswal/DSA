4d372124d5bfeb15fad37cb9c0c492a05e9d32e9
rehanjaswal <rehanxjaswal@gmail.com>
2026-08-18 15:56:53 +0000

    carry forward technique

--- 12_sliding_window_two_pointers/01_carry_forward_technique.cpp
+++ 12_sliding_window_two_pointers/01_carry_forward_technique.cpp
@@ -5,6 +5,7 @@ int main() {
     cin >> n;
     vector<int> arr(n);
     for (int i = 0; i < n; i++) cin >> arr[i];
+    
     for (int l = 0; l < n; l++) {
         int sum = 0;
         for (int r = l; r < n; r++) {
