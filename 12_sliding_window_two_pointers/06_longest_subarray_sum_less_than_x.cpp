189a05c831374290aeb4d3219c22863b0238c5b9
rehanjaswal <rehanxjaswal@gmail.com>
2026-08-19 23:58:12 +0000

    subarray problem

---
+++ 12_sliding_window_two_pointers/06_longest_subarray_sum_less_than_x.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+
+    int l = 0, sum = 0, maxLength = 0;
+    for (int r = 0; r < n; r++) {
+        sum += nums[r];
+        while (sum >= x) {
+            sum -= nums[l];
+            l++;
+        }
+        maxLength = max(maxLength, r - l + 1);
+    }
+    cout << maxLength << endl;
+return 0;
+}
